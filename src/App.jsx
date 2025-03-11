d3fab928ac70e7978eadb9e60e64bd0a06cfcf35
dnjswlekd <wj8014@gmail.com>
2025-03-11 22:35:55 +0900

    todo input 창 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todo, setTodo] = useState([
-    { id: 1, title: '아침 챙겨먹기', completed: true },
-    { id: 2, title: '점심 챙겨먹기', completed: false },
-    { id: 3, title: '저녁 챙겨먹기', completed: false },
-  ]);
+  const [todo, setTodo] = useState([]);
 
   return (
     <>
+      {/* fragment로 꼭 감싸기 <> (빈껍데기) */}
       <div className="App">
-        {todo.map((t, id) => (
-          <div key={id}>{t.title}</div>
-        ))}
+        {todo.length === 0 ? (
+          <div>
+            <h1>할 일을 추가해보세요.</h1>
+            <input type="text" />
+          </div>
+        ) : (
+          <>
+            {todo.map((t, id) => (
+              <div key={id}>{t.title}</div>
+            ))}
+            <input type="text" />
+          </>
+        )}
       </div>
     </>
   );
