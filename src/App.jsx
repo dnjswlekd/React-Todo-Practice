14f8ee066ec1d443453aef892b255e0199605775
dnjswlekd <wj8014@gmail.com>
2025-03-11 22:22:40 +0900

    useState, 반복문 .map

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todo, setTodo] = useState();
+  const [todo, setTodo] = useState([
+    { title: '아침 챙겨먹기', completed: true },
+    { title: '점심 챙겨먹기', completed: false },
+    { title: '저녁 챙겨먹기', completed: false },
+  ]);
 
   return (
     <>
       <div className="App">
-        <h1>todolist</h1>
+        {todo.map((t, index) => (
+          <div key={index}>{t.title}</div>
+        ))}
       </div>
     </>
   );
