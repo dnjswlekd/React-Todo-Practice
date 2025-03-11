53ee0006790967a7a7cd6af58e0e06c03a50d968
dnjswlekd <wj8014@gmail.com>
2025-03-11 22:25:21 +0900

    key index에서 id로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import './App.css';
 
 function App() {
   const [todo, setTodo] = useState([
-    { title: '아침 챙겨먹기', completed: true },
-    { title: '점심 챙겨먹기', completed: false },
-    { title: '저녁 챙겨먹기', completed: false },
+    { id: 1, title: '아침 챙겨먹기', completed: true },
+    { id: 2, title: '점심 챙겨먹기', completed: false },
+    { id: 3, title: '저녁 챙겨먹기', completed: false },
   ]);
 
   return (
     <>
       <div className="App">
-        {todo.map((t, index) => (
-          <div key={index}>{t.title}</div>
+        {todo.map((t, id) => (
+          <div key={id}>{t.title}</div>
         ))}
       </div>
     </>
